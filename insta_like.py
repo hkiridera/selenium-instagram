a5787b7cd4b15e74e4fd1b081a80d5d73964d09c
hkiridera <kiridera.corp@gmail.com>
2017-11-06 08:48:37 +0900

    dom要素変更されたので修正

--- insta_like.py
+++ insta_like.py
@@ -7,19 +7,18 @@ driver.get('https://www.instagram.com/?hl=ja')    #インスタを開く
 time.sleep(1)                                           #ページを開くまで時間がかかるので取り敢えず１秒停止
 driver.find_element_by_xpath('//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/span/button').click()  #ログインボタンをxpathで指定
 time.sleep(1)                                           #ページを開くまで時間がかかるので取り敢えず１秒停止
-driver.find_element_by_xpath('//*[@id="email"]').send_keys('')  #メールアドレス
-driver.find_element_by_xpath('//*[@id="pass"]').send_keys('')  #メールアドレス
-driver.find_element_by_xpath('//*[@id="loginbutton"]').click()  #ログインボタンをxpathで指定
+#driver.find_element_by_xpath('//*[@id="email"]').send_keys('')  #メールアドレス
+#driver.find_element_by_xpath('//*[@id="pass"]').send_keys('')  #メールアドレス
+#driver.find_element_by_xpath('//*[@id="loginbutton"]').click()  #ログインボタンをxpathで指定
 
 
-
-time.sleep(6)                                           #ページを開くまで時間がかかるので取り敢えず6秒停止
+time.sleep(30)                                           #ページを開くまで時間がかかるので取り敢えず6秒停止
 
 for i in range(1,1000):
   driver.get('https://www.instagram.com/')  #個別のページを開く
   time.sleep(1)                                           #ページを開くまで時間がかかるので取り敢えず１秒停止
   try:
-    driver.find_element_by_xpath('//*[@id="mainFeed"]/div/div/div[1]/article[1]/div[2]/section[1]/a[1]/span').click()  #like
+    driver.find_element_by_xpath('//*[@id="mainFeed"]/div/div/div[1]/div/article[1]/div[2]/section[1]/a[1]/span').click()  #like
 #    time.sleep(3600*1)
     time.sleep(60*2)
   except KeyboardInterrupt  :
